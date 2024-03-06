9efe5023f4c8c70621cea4a5979a1f471bc473ae
AuracleTech <VirgilFabre@outlook.com>
2024-03-06 03:26:56 -0500

    Improved content scrolling experience

--- TS/experiment.ts
+++ TS/experiment.ts
@@ -4,11 +4,30 @@ import { Panels, Panel } from "./panels.ts";
 const panels = new Panels();
 document.body.append(panels);
 
+// Overflow
+
+const panel_overflow_content = panels.new_panel({
+	resizable: true,
+	preservable: true,
+	spawn_at_random: false,
+	spawn_at_cursor: false,
+});
+const overflow_wrapper = document.createElement("div");
+overflow_wrapper.classList.add("overflow-wrapper");
+const overflow_content = document.createElement("div");
+overflow_content.classList.add("overflow-content");
+overflow_wrapper.append(overflow_content);
+panel_overflow_content.content.append(overflow_wrapper);
+
+// Text
+
 const panel_text: Panel = panels.new_panel();
 const text = document.createElement("div");
 text.classList.add("text");
 text.textContent = "Hello, world!";
 panel_text.content.append(text);
+
+// Resizable with fixed content
 
 const panel_image: Panel = panels.new_panel({
 	resizable: true,
@@ -19,6 +38,8 @@ const panel_image: Panel = panels.new_panel({
 const image = document.createElement("div");
 image.classList.add("random-image");
 panel_image.content.append(image);
+
+//
 
 const panel_icons: Panel = panels.new_panel();
 const icons = document.createElement("div");
